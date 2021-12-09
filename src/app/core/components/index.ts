f076d2f91ee753754210e3be96d2dc73b5b2f11c
Ivan Mihajlovic <ivan.mihajlovic@escriba.de>
2021-12-09 16:01:57 +0100

    Add breadcrumb component

--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,9 +1,12 @@
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
+export * from './breadcrumbs/breadcrumbs.component';
 export * from './page-not-found/page-not-found.component';
 export * from './site-header/site-header.component';
 
 export const CORE_COMPONENTS = [
+  BreadcrumbsComponent,
   PageNotFoundComponent,
   SiteHeaderComponent
 ];
